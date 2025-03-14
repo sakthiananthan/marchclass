c20a63fe23a258c7edd90d413dd46bd6684e9782
sakthisakthiananthan@gmail.com <ssstech2708@gmail.com>
2025-03-14 09:14:22 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,19 @@
+
+test_dict={
+    1:{
+        "name":"sakthi",
+        "age":"30",
+        "course":"python",
+        "address":"Tuty"
+    },
+        2:{
+        "name":"anand",
+        "age":"30",
+        "course":"python",
+        "address":"chennai"
+    }
+}
+
+for k,v in test_dict.items():
+    print(k)
+    print(v)
